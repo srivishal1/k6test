d9a2826f688b9cdb6e56b7e8823ec48b6284c497
vishal srivastava <vishal.srivastava@modmed.com>
2022-12-13 13:15:24 +0530

    modified env

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,11 +1,8 @@
 export default class Utils {
 
   static getBaseUrl() {
-    switch (process.env.NODE_ENV) {
-      case 'production':
+   
         return 'https://serverest.dev'
-      default:
-        console.error('Url or env not defined')
+     
     }
   }
-}
